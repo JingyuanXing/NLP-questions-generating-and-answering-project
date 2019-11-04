2094143d9403ecb929ae86f1eb828c08ba3311c2
JingyuanXing <lucyxing0217@gmail.com>
2019-11-03 21:09:36 -0500

    NER, POS tagging

--- questions_answering.py
+++ questions_answering.py
@@ -1,4 +1,4 @@
-
+import spacy
 
 questions = ['What is ?', 
 'what does the period from the Third Dynasty through to the Sixth Dynasty is ?', 
@@ -14,3 +14,44 @@ questions = ['What is ?',
 'The Old Kingdom is regarded as the period from the Third what organization through to the Sixth Dynasty ?', 
 'The Old Kingdom is regarded as the period from the Third Dynasty through to the what organization Dynasty ?', 
 'The Old Kingdom is regarded as the period from the Third what organization through to the Sixth Dynasty ?'] 
+
+r = open(file = "Development_data/set1/a1.txt", mode = "r")
+text = r.read()
+paragraphs = text.split('\n')
+current_para = paragraphs[3]
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(current_para)
+
+
+### POS tagging
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_)
+print("\n")
+
+for ques in questions:
+    q = nlp(ques)
+    for token in q:
+        print(token.text, token.lemma_, token.pos_, token.tag_)
+print("\n")
+
+
+### NER tagging
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+print("\n")
+
+for ques in questions:
+    q = nlp(ques)
+    for ent in q.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+print("\n")
+
+
+
+
+
+
+
+
+
