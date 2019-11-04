c511ef60eadb73d0eef09cba4375fb9798bdcfbf
JingyuanXing <lucyxing0217@gmail.com>
2019-11-03 19:40:13 -0500

    answering

---
+++ questions_answering.py
@@ -0,0 +1,16 @@
+
+
+questions = ['What is ?', 
+'what does the period from the Third Dynasty through to the Sixth Dynasty is ?', 
+'what organization period from the Third Dynasty through to the Sixth Dynasty is ?', 
+'the period from the what organization Dynasty through to the Sixth Dynasty is ?', 
+'the period from the Third what organization through to the Sixth Dynasty is ?', 
+'the period from the Third Dynasty through to the what organization Dynasty is ?', 
+'the period from the Third what organization through to the Sixth Dynasty is ?', 
+'What is regarded as the period from the Third Dynasty through to the Sixth Dynasty ?', 
+'what does The Old Kingdom is regarded as ?', 
+'The Old Kingdom is regarded as what organization period from the Third Dynasty through to the Sixth Dynasty ?', 
+'The Old Kingdom is regarded as the period from the what organization Dynasty through to the Sixth Dynasty ?', 
+'The Old Kingdom is regarded as the period from the Third what organization through to the Sixth Dynasty ?', 
+'The Old Kingdom is regarded as the period from the Third Dynasty through to the what organization Dynasty ?', 
+'The Old Kingdom is regarded as the period from the Third what organization through to the Sixth Dynasty ?'] 
